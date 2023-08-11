863c9d183cda5fb532efcd1cfc6886c8a1be776e
Daniel Caicedo <97268461+DANDIERU@users.noreply.github.com>
2023-08-11 18:44:11 -0500

    Subi el lab1

---
+++ Laboratorio 1/dependencia.js
@@ -0,0 +1,7 @@
+import trash from "trash";
+//await trash(['nombre-del-archivo']);
+// Ya lo intente con varias imagenes
+
+await trash(['']);
+
+
---
+++ Laboratorio 1/dependencia2.js
@@ -0,0 +1,7 @@
+
+import calculator from 'birthday-calculator';
+ 
+
+const date = new Date('2002,07,14');
+const result = calculator.getDayWhenBorn(date);
+console.log(result);
